27fe995df1ffb96d18f5055e4975bf84463efc0e
illusionist nemo <illusionist.nemo@gmail.com>
2019-05-08 23:30:28 +0300

    add data generator

---
+++ DataGenerator/DataGenerator.cs
@@ -0,0 +1,46 @@
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using DbModel;
+using DbModel.DbTypes;
+using EfProvider;
+
+namespace DataGenerator
+{
+    public class DataGenerator
+    {
+        private readonly IDataProvider _provider;
+        private readonly string _environment;
+
+        public DataGenerator(IDataProvider provider, string environment)
+        {
+            _provider = provider;
+            _environment = environment;
+        }
+
+        public async Task Gen()
+        {
+            using (var transaction = _provider.Transaction())
+            {
+                await User().ConfigureAwait(false);
+                transaction.Commit();
+            }
+        }
+
+        private Task User()
+        {
+            var users = new List<UserDb>();
+            for (var i = 0; i < 10; i++)
+            {
+                var userDb = new UserDb
+                {
+                    Login = "Test" + i,
+                    Pasword = "123456",
+                    Profile = new UserProfileDb {Gender = Gender.Undefined}
+                };
+                users.Add(userDb);
+            }
+
+            return _provider.BatchInsert(users);
+        }
+    }
+}
---
+++ DataGenerator/Program.cs
@@ -0,0 +1,49 @@
+﻿using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using DbModel.Store;
+using EfProvider;
+using JetBrains.Annotations;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Mono.Options;
+
+namespace DataGenerator
+{
+    [UsedImplicitly(ImplicitUseKindFlags.InstantiatedWithFixedConstructorSignature)]
+    public static class GeneratorProgram
+    {
+        public static string EnvironmentName = Microsoft.AspNetCore.Hosting.EnvironmentName.Development;
+        public static string BaseDirectory;
+
+        [UsedImplicitly]
+        private static Task Main(string[] args)
+        {
+            if (args.Any())
+            {
+                var dataOptions = new OptionSet
+                {
+                    {"environment=", s => EnvironmentName = s}
+                };
+                dataOptions.Parse(args);
+            }
+
+            var configurationBuilder = new ConfigurationBuilder()
+                .SetBasePath(BaseDirectory ?? Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json", true, true)
+                .AddJsonFile($"appsettings.{EnvironmentName}.json", true)
+                .AddJsonFile("appsettings.local.json", true);
+
+            var configuration = configurationBuilder.Build();
+            var builder = new DbContextOptionsBuilder()
+                .UseNpgsql(configuration["DbConnection"], n => n.MigrationsAssembly("DbMigrator"));
+
+            var modelStore = new ModelStore();
+            var context = new CustomEfDbContext(modelStore, builder.Options);
+            var provider = new EfDataProvider(context);
+
+            var generator = new DataGenerator(provider, EnvironmentName);
+            return generator.Gen();
+        }
+    }
+}
